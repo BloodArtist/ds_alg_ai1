2c1cb070b4627a7a0d99a4db69509aabc1342cdf
BloodArtist <yossefavila@hotmail.com>
2024-04-02 13:50:37 -0600

    Comments updated

--- deps/include/ABGS_MemoryManager/abgs_platform_types.h
+++ deps/include/ABGS_MemoryManager/abgs_platform_types.h
@@ -13,17 +13,17 @@
 #include <stdint.h>
 
 typedef uint8_t 		  u8;
-typedef int_least8_t 	s8;
+typedef int_least8_t  s8;
 typedef uint16_t 		  u16;
 typedef int_least16_t s16;
 typedef uint32_t 		  u32;
 typedef int_least32_t s32;
 typedef uint64_t 		  u64;
-typedef int_least64_t	s64;
+typedef int_least64_t s64;
 
 typedef uint8_t 		  bool;
 #define true 			    1
-#define false 			  0
+#define false 				0
 
 typedef uint32_t		BIT_FIELD;
 
